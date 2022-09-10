3bd320e7c8f7c35d481da8d367d42a5fa15155c5
Okeowo Yusuf <luminousabayomi@gmail.com>
2022-09-09 19:18:39 -0700

    Task 7 version 1

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - print lower case alphabet in reverse order
+ *
+ * Return: Always (Success)
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'z'; c >= 'a'; c--)
+	{
+		putchar(c);
+	}
+
+	putchar('\n');
+	return (0);
+}
